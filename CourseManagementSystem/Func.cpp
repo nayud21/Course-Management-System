b24040cb175d76f75a238c7059376b0ce4b5cac2
nayud21 <buiduyan2116@gmail.com>
2023-05-19 22:46:49 +0700

    third commit

--- CourseManagementSystem/Func.cpp
+++ CourseManagementSystem/Func.cpp
@@ -17,39 +17,6 @@ void Login() {
 		}
 	}
 	 }
-//void khoitaodanhsach(list& l) {
-//	l.pHEAD = NULL;
-//	l.pTAIL = NULL;
-//}
-//NODE* KhoitaoNODE(SINHVIEN x) {
-//	NODE* p = new NODE;
-//	if (p == 0) {
-//		cout << "\nCap phat du lieu that bai!";
-//		return 0;
-//	}
-//	p->data= x;
-//	p->pNext = NULL;
-//	return p;
-//}
-//void ThemDau(list& l, NODE* p) {
-//	if (l.pHEAD = NULL) {
-//		l.pHEAD = l.pTAIL = p;
-//	}
-//	else {
-//		p->pNext = l.pHEAD;
-//		l.pHEAD = p;
-//	}
-//}
-//void ThemCuoi(list& l, NODE* p) {
-//	if (l.pHEAD == 0) {
-//		l.pHEAD = l.pTAIL = p;
-//	}
-//	else
-//	{
-//		l.pTAIL->pNext = p;
-//		l.pTAIL = p;
-//	}
-//}
 void ReadDate(ifstream &filein,Date &date) {
 	filein >> date.Day;
 	filein.seekg(1, 1);//dich sang phai 1 byte
@@ -57,18 +24,18 @@ void ReadDate(ifstream &filein,Date &date) {
 	filein.seekg(1, 1);//dich sang phai 1 byte
 	filein >> date.Year;
 }
-void ReadSinhvien(ifstream &filein, SINHVIEN &sv) {
-	getline(filein, sv.Name, ',');
-	//filein.seekg(1, 1);
-	getline(filein, sv.StudentID, ',');
-	//filein.seekg(1, 1);
-	ReadDate(filein, sv.ngaysinh);
-}
-void Output(SINHVIEN sv) {
-	cout << "\nHo ten: " << sv.Name;
-	cout << "\nMSSV: " << sv.StudentID;
-	cout << "\nNgay sinh: " << sv.ngaysinh.Day << "/" << sv.ngaysinh.Month << "/" << sv.ngaysinh.Year<<endl;
-}
+//void ReadSinhvien(ifstream &filein, Student &sv) {
+//	getline(filein, sv.Name, ',');
+//	//filein.seekg(1, 1);
+//	getline(filein, sv.StudentID, ',');
+//	//filein.seekg(1, 1);
+//	ReadDate(filein, sv.ngaysinh);
+//}
+//void Output(Student sv) {
+//	cout << "\nHo ten: " << sv.Name;
+//	cout << "\nMSSV: " << sv.StudentID;
+//	cout << "\nNgay sinh: " << sv.ngaysinh.Day << "/" << sv.ngaysinh.Month << "/" << sv.ngaysinh.Year<<endl;
+//}
 void Registration() {
 
 }
@@ -115,4 +82,192 @@ void Menu_Staff() {
 }
 void Menu_Login() {
 
+}
+//
+int CountStudent() {
+	int count = 0;
+	ifstream infile;
+	infile.open("Students.csv");
+	if (!infile.is_open()) {
+		return 0;
+	}
+	string line;
+	getline(infile, line);
+	while (infile) {
+		getline(infile, line);
+		count++;
+	}
+	infile.close();
+	return count;
+}
+int StringToInteger(string s) {
+	int n = s.length();
+	int res = 0;
+	for (int i = 0; i < n; i++) {
+		switch (s[n - 1 - i]) {
+		case '0': res += 0 * pow(10, i); break;
+		case '1': res += 1 * pow(10, i); break;
+		case '2': res += 2 * pow(10, i); break;
+		case '3': res += 3 * pow(10, i); break;
+		case '4': res += 4 * pow(10, i); break;
+		case '5': res += 5 * pow(10, i); break;
+		case '6': res += 6 * pow(10, i); break;
+		case '7': res += 7 * pow(10, i); break;
+		case '8': res += 8 * pow(10, i); break;
+		case '9': res += 9 * pow(10, i); break;
+		}
+	}
+	return res;
+}
+void ReadStudent(Student*& S, int& n) {
+	S = new Student[CountStudent()];
+	ifstream file("Students.csv");
+	if (!file.is_open()) {
+		cout << "Cannot open file." << endl;
+		return;
+	}
+	string line;
+	string SNo;
+	string SSocialID;
+	string SDate;
+	string day;
+	string month;
+	string year;
+	int i = 0;
+	getline(file, line);
+	while (file) {
+		getline(file, SNo, ',');
+		getline(file, S[i].StudentID, ',');
+		getline(file, S[i].FirstName, ',');
+		getline(file, S[i].LastName, ',');
+		getline(file, S[i].Gender, ',');
+		getline(file, day, '/');
+		getline(file, month, '/');
+		getline(file, year, ',');
+		getline(file, SSocialID, ',');
+		getline(file, S[i].Class, ',');
+		getline(file, S[i].Pass);
+
+		S[i].No = StringToInteger(SNo);
+		S[i].DateOfBirth.Day = StringToInteger(day);
+		S[i].DateOfBirth.Month = StringToInteger(month);
+		S[i].DateOfBirth.Year = StringToInteger(year);
+		S[i].SocialID = StringToInteger(SSocialID);
+		i++;
+	}
+	n = i - 1;
+	file.close();
+}
+void Print_ListStudent(Student* S, int n) {
+	for (int i = 0; i < n; i++) {
+		cout << "Student." << endl;
+		cout << "No: " << S[i].No << endl;
+		cout << "Student ID: " << S[i].StudentID << ". F & L Name: " << S[i].FirstName << " " << S[i].LastName << endl;
+		cout << "Gender: " << S[i].Gender << ". Date of Birth: " << S[i].DateOfBirth.Day << "/" << S[i].DateOfBirth.Month << "/" << S[i].DateOfBirth.Year << endl;
+		cout << "Social ID: " << S[i].SocialID << ". Class: " << S[i].Class << endl;
+	}
+}
+void Print_ElementStudent(Student* S, int i) {
+	cout << "No: " << S[i].No << endl;
+	cout << "StudentID: " << S[i].StudentID << ". F & L Name: " << S[i].FirstName << " " << S[i].LastName << endl;
+	cout << "Gender: " << S[i].Gender << ". Date of Birth: " << S[i].DateOfBirth.Day << "/" << S[i].DateOfBirth.Month << "/" << S[i].DateOfBirth.Year << endl;
+	cout << "Social ID: " << S[i].SocialID << ". Class: " << S[i].Class << endl;
+}
+
+int CountTeacher() {
+	int count = 0;
+	ifstream infile;
+	infile.open("Teachers.csv");
+	if (!infile.is_open()) {
+		return 0;
+	}
+	string line;
+	getline(infile, line);
+	while (infile) {
+		getline(infile, line);
+		count++;
+	}
+	infile.close();
+	return count;
+}
+void Read_Teacher(Teacher*& T, int& n) {
+	T = new Teacher[CountTeacher()];
+	ifstream file("Teachers.csv");
+	if (!file.is_open()) {
+		cout << "Cannot open file." << endl;
+		return;
+	}
+	string line;
+	string SNo;
+	string SSocialID;
+	int i = 0;
+	getline(file, line);
+	while (file) {
+		getline(file, SNo, ',');
+		getline(file, T[i].TeacherID, ',');
+		getline(file, T[i].FirstName, ',');
+		getline(file, T[i].LastName, ',');
+		getline(file, T[i].Gender, ',');
+		getline(file, SSocialID, ',');
+		getline(file, T[i].Faculty, ',');
+		getline(file, T[i].Pass);
+
+		T[i].No = StringToInteger(SNo);
+		T[i].SocialID = StringToInteger(SSocialID);
+		i++;
+	}
+	n = i - 1;
+	file.close();
+}
+void Print_ListTeacher(Teacher* T, int m) {
+	for (int i = 0; i < m; i++) {
+		cout << "Teacher." << endl;
+		cout << "No: " << T[i].No << endl;
+		cout << "Teacher ID: " << T[i].TeacherID << ". F & L Name: " << T[i].FirstName << " " << T[i].LastName << endl;
+		cout << "Gender: " << T[i].Gender << ". SocialID: " << T[i].SocialID << ". Fal: " << T[i].Faculty << endl;
+	}
+}
+void Print_ElementTeacher(Teacher* T, int i) {
+	cout << "Teacher." << endl;
+	cout << "No: " << T[i].No << endl;
+	cout << "Teacher: " << T[i].TeacherID << ". F & L Name: " << T[i].FirstName << " " << T[i].LastName << endl;
+	cout << "Gender: " << T[i].Gender << ". Social ID: " << T[i].SocialID << ". Fal: " << T[i].Faculty << endl;
+}
+
+
+bool CheckPass_Student(Student* S, int n, int& Pos, string User, string Pass) {
+	for (int i = 0; i < n; i++) {
+		Pos = i;
+		if (S[i].StudentID == User && S[i].Pass == Pass)
+			return 1;
+	}
+	return 0;
+}
+bool CheckPass_Teacher(Teacher* T, int m, int& Pos, string User, string Pass) {
+	for (int i = 0; i < m; i++) {
+		Pos = i;
+		if (T[i].TeacherID == User && T[i].Pass == Pass)
+			return 1;
+	}
+	return 0;
+}
+void Paint(int x, int y, string a, int color) {
+	Gotoxy(x, y);
+	Color(color);
+	cout << a;
+}
+void Color(int i) {
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), i);
+}
+void Gotoxy(int x, int y) {
+	HANDLE color;
+	color = GetStdHandle;
+	COORD coor = { x,y };
+	SetConsoleCursorPosition(color, coor);
+}
+void Input_Account(string& User, string& Pass) {
+	Paint(15, 20, "ID of User: ", 10);
+	getline(cin, User);
+	Paint(15, 20, "Password: ", 10);
+	getline(cin, Pass);
 }
--- CourseManagementSystem/Header.h
+++ CourseManagementSystem/Header.h
@@ -1,7 +1,11 @@
 #pragma once
-#include<iostream>
-#include<fstream>
-#include<string>
+#include <iostream>
+#include <conio.h>
+#include <fstream>
+#include <windows.h>
+#include <string>
+#include <cmath>
+#include <ctime>
 using namespace std;
 struct DateOfBirth {
 	int Day;
@@ -9,33 +13,103 @@ struct DateOfBirth {
 	int Year;
 };
 typedef DateOfBirth Date;
-struct SINHVIEN {
+struct Student {
 	int No;
 	string StudentID;
-	string Name;
-	char Gender[4];
-	Date ngaysinh;
-	char* SocialId;
+	string FirstName;
+	string LastName;
+	string Gender;
+	Date DateOfBirth;
+	string SocialID;
+	string Class;
+	string Pass;
+
 };
-//struct node {
-//	SINHVIEN data;
-//	NODE *pNext;
-//};
-//typedef struct node NODE;
-//struct list {
-//	NODE* pHEAD;
-//	NODE* pTAIL;
-//};
-//void khoitaodanhsach(list& l);
-//NODE* KhoitaoNODE(SINHVIEN x);
-//void ThemCuoi(list& l, NODE* p);
-//void ThemDau(list& l, NODE* p);
+struct Teacher {
+	int No;
+	string TeacherID;
+	string FirstName;
+	string LastName;
+	string Gender;
+	int SocialID;
+	string Faculty;
+	string Pass;
+};
+struct Courses {
+	int Credits;
+	int MaxStu;
+	int DayBegin;
+	int MonthBegin;
+	int DayEnd;
+	int MonthEnd;
+	string ID;
+	string Class_Name;
+	string Teacher_Name;
+	string Day1;
+	string Session1;
+	string Day2;
+	string Session2;
+};
+struct Student_Courses {
+	int No;
+	string StudentID;
+	string Firstname;
+	string LastName;
+	string Gender;
+	string Class;
+	string CourseID;
+	string Classname;
+	int Credits;
+	string TeacherName;
+	string Day1;
+	string Session1;
+	string Day2;
+	string Session2;
+	int DayBegin;
+	int MonthBegin;
+	int DayEnd;
+	int MonthEnd;
+	float OtherMark;
+	float MidtermMark;
+	float FinalMark;
+	float TotalMark;
+};
+struct Classes {
+	string NameClass;
+	int Num;
+};
+
+
+
+//
+int StringToInteger(string s);
+int CountStudent();
+void ReadStudent(Student*& S, int& n);
+void Print_ListStudent(Student* S, int n);
+void Print_ElementStudent(Student* S, int i);
+//
+int CountTeacher();
+void Read_Teacher(Teacher*& T, int& n);
+void Print_ListTeacher(Teacher* T, int m);
+void Print_ElementTeacher(Teacher* T, int i);
+//
+bool CheckPass_Teacher(Teacher* T, int m, int& pos, string user, string Pass);
+bool CheckPass_Student(Student* S, int n, int& pos, string user, string Pass);
+//
+void Paint(int x, int y, string a, int color);
+void Input_Account(string& User, string& Pass);
+void Color(int i);
+void Gotoxy(int x, int y);
+//
+
+
+
 void Login();
 void Registration();
 void ForgotPassword();
 void ReadDate(ifstream& filein, Date& date);
-void ReadSinhvien(ifstream& filein, SINHVIEN& sv);
-void Output(SINHVIEN sv);
+void ReadSinhvien(ifstream& filein, Student& sv);
+void Output(Student sv);
 void Menu_Student();
 void Menu_Staff();
 void Menu_Login();
